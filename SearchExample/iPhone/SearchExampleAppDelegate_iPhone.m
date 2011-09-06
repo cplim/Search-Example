f25348e0774f9f03e12241198e568c3aa8f55eba
CP Lim <cheenpin.lim@gmail.com>
2011-09-06 21:44:11 +1000

    Replaced template variable with correct value

--- SearchExample/iPhone/SearchExampleAppDelegate_iPhone.m
+++ SearchExample/iPhone/SearchExampleAppDelegate_iPhone.m
@@ -3,7 +3,7 @@
 //  SearchExample
 //
 //  Created by C.P. Lim on 6/09/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 C.P. Lim. All rights reserved.
 //
 
 #import "SearchExampleAppDelegate_iPhone.h"
--- SearchExampleTests/SearchExampleTests.h
+++ SearchExampleTests/SearchExampleTests.h
@@ -3,7 +3,7 @@
 //  SearchExampleTests
 //
 //  Created by C.P. Lim on 6/09/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 C.P. Lim. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
